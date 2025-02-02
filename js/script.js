3390d703794d234c42260043dba77dc0c69bf48e
Dhruv Atrey <dhruvatrey@gmail.com>
2025-02-02 20:06:16 +0530

    JS Final

--- js/script.js
+++ js/script.js
@@ -1,19 +1,42 @@
-const menu = document.getElementById("menu");
+const menuButton = document.getElementById("menu");
 const nav = document.getElementById("nav");
-var navIsOpen = false
+var navIsOpen = false;
 
-menu.addEventListener('click', function(e){
-    e.preventDefault();
-    toggleNav();
-})
+menuButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  toggleNav();
+});
 
-var toggleNav = function() {
-    if (navIsOpen){
-        navIsOpen=false;
-        nav.classList.remove("mobile-nav");
-    } else {
-        navIsOpen=true;
-        nav.classList.add("mobile-nav");
+var toggleNav = function () {
+  if (navIsOpen) {
+    navIsOpen = false;
+    nav.classList.remove("mobile-nav");
+  } else {
+    navIsOpen = true;
+    nav.classList.add("mobile-nav");
+  }
+};
 
-    }
-}
+const contactButton = document.getElementById("nav-contact-button");
+const popup = document.getElementById("popup");
+const contactForm = document.getElementById("contact-form");
+let popupIsOpen = false;
+
+contactButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (!popupIsOpen) {
+    popup.classList.remove("hidden");
+    document.getElementsByTagName("body")[0].classList.add("stop-scrolling");
+    popupIsOpen = true;
+  }
+});
+
+const popupCloseButton = document.getElementById("popup-close");
+
+popupCloseButton.addEventListener("click", function (e) {
+  if (popupIsOpen) {
+    popup.classList.add("hidden");
+    popupIsOpen = false;
+    document.getElementsByTagName("body")[0].classList.remove("stop-scrolling");
+  }
+});
